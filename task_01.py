2d8bbc5b425aeba216125e91302a2d0eb2d9b8b6
Nicole Parrilla <nicole.parrilla@spsmail.cuny.edu>
2016-03-04 02:50:22 +0000

    Made lists from tuples

---
+++ task_01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Creating lists with tuples"""
+
+ELEMENTS = (None,
+            'Hydrogen',
+            'Helium',
+            'Lithium',
+            'Beryllium',
+            'Boron',
+            'Carbon')
+
+OPERATIONS_ORDER = ['Parentheses',
+                    'Exponents',
+                    'Multiplication',
+                    'Division',
+                    'Addition',
+                    'Subtraction']
